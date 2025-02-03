b423df505a1a996d38f99c92bb2dec3ca3743339
B3SUS <IB3SUSI@gmail.com>
2025-02-03 11:04:32 +0300

    v1 back

--- src/validator/validator.controller.ts
+++ src/validator/validator.controller.ts
@@ -1,22 +1,32 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import * as EmailValidator from 'email-validator';
-
 const WAValidator = require('multicoin-address-validator');
-
 
 @Controller('validator')
 export class ValidatorController {
     @Post('email')
-    async validateEmail(@Body('email') email: string
-    ) {
-        return EmailValidator.validate(email)
+    async validateEmail(@Body('email') email: string) {
+        return EmailValidator.validate(email);
     }
 
     @Post('address')
     async validateAddress(
-        @Body('address') address: string,
-        @Body('coin') coin: string,
-    ){
-        return WAValidator.validate(address, coin);
+      @Body('address') address: string,
+      @Body('coin') coin: string,
+    ) {
+        if (!address || typeof address !== 'string' || address.trim() === '') {
+            throw new Error('Invalid address');
+        }
+
+        if (!coin || typeof coin !== 'string' || coin.trim() === '') {
+            throw new Error('Invalid coin type');
+        }
+
+        try {
+            return WAValidator.validate(address, coin);
+        } catch (error) {
+            console.error('Error during address validation:', error);
+            throw new Error('Address validation failed');
+        }
     }
 }
