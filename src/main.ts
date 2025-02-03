0165471823ad9ab422de4f14982ef414da89e830
B3SUS <IB3SUSI@gmail.com>
2025-02-03 09:38:07 +0300

    v1 back

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://crypta.space'],
+    origin: 'http://crypta.space',
     methods: 'GET,POST',
     credentials: true,
   });
