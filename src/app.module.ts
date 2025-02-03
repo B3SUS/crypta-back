4df150588b004594817b6ff8f4ac71963aa8fd1e
B3SUS <IB3SUSI@gmail.com>
2025-02-03 10:31:23 +0300

    v1 back

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,14 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { CoinsModule } from './coins/coins.module';
 import { ValidatorController } from './validator/validator.controller';
 import { ValidatorService } from './validator/validator.service';
-import { ValidatorModule } from './validator/validator.module';
 import { HttpModule } from "@nestjs/axios";
+import { CoinsController } from "./coins/coins.controller";
+import { CoinsService } from "./coins/coins.service";
 
 @Module({
   imports: [HttpModule],
-  controllers: [AppController, ValidatorController],
-  providers: [AppService, ValidatorService],
+  controllers: [CoinsController, ValidatorController],
+  providers: [CoinsService, ValidatorService],
 })
 export class AppModule {
-  onModuleInit() {
-
-  }
 }
