c52f52d98dc86ff1f280870fb006b1498458f2fa
B3SUS <IB3SUSI@gmail.com>
2025-02-03 10:59:17 +0300

    v1 back

--- src/coins/coins.service.ts
+++ src/coins/coins.service.ts
@@ -26,46 +26,51 @@ export class CoinsService {
     ];
 
     private readonly neededRates = [
-      'russian-ruble',
-      'georgian-lari'
+
     ];
 
 
     constructor(private readonly httpService: HttpService) {}
 
-    async getCoins(): Promise<any>{
-        const apiUrl = `https://api.coincap.io/v2/assets/?ids=${this.neededCoins.join(',')}`
-        try{
-            const coinsResponse = await firstValueFrom(this.httpService.get(apiUrl))
+    async getCoins(): Promise<any> {
+        const apiUrl = `https://api.coincap.io/v2/assets/?ids=${this.neededCoins.join(',')}`;
+        try {
+            console.log('Fetching coins data from:', apiUrl);
+            const coinsResponse = await firstValueFrom(this.httpService.get(apiUrl));
+            console.log('Coins response data:', coinsResponse.data);  // Логируем весь ответ
 
             const coinDataPromises = this.neededRates.map((coin) => {
                 const apiUrl = `https://api.coincap.io/v2/rates/${coin}`;
+                console.log('Fetching rates data from:', apiUrl);
                 return firstValueFrom(this.httpService.get(apiUrl));
             });
 
             const responses = await Promise.all(coinDataPromises);
+            console.log('Rates responses:', responses);
 
             const ratesResponse = responses.map(response => response.data.data);
 
-            const updatedCoinsResponse = coinsResponse.data.data.map(({rank, supply, maxSupply, marketCapUsd, volumeUsd24Hr, changePercent24Hr, vwap24Hr, explorer, id, symbol, name, priceUsd}) =>({
+            const updatedCoinsResponse = coinsResponse.data.data.map(({rank, supply, maxSupply, marketCapUsd, volumeUsd24Hr, changePercent24Hr, vwap24Hr, explorer, id, symbol, name, priceUsd}) => ({
                 id: id,
                 symbol: symbol,
                 name: name,
                 priceUsd: priceUsd,
                 type: 'coin'
             }));
-            const updatedRatesResponse = ratesResponse.map(({symbol, currencySymbol, rateUsd, id, type}, index)=>({
+
+            const updatedRatesResponse = ratesResponse.map(({symbol, currencySymbol, rateUsd, id, type}) => ({
                 id: id,
                 symbol: symbol,
-                name: 'Фиат '+ symbol,
+                name: 'Фиат ' + symbol,
                 priceUsd: rateUsd,
                 type: type
             }));
 
-            return updatedCoinsResponse.concat(updatedRatesResponse)
-        }
-        catch (error){
-            throw new Error(error.message)
+            return updatedCoinsResponse.concat(updatedRatesResponse);
+        } catch (error) {
+            console.error('Error in getCoins method:', error);  // Логируем ошибку
+            throw new Error('Error fetching coins data: ' + error.message);
         }
     }
+
 }
